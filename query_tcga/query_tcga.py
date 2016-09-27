8dc89d24c0aba5ff0e2dee4aa40aa8b805e192e6
Jacqueline Buros Novik <jackinovik@gmail.com>
2016-09-27 00:06:27 -0400

    store submitter_id in clinical data

--- query_tcga/query_tcga.py
+++ query_tcga/query_tcga.py
@@ -465,12 +465,13 @@ def get_clinical_data_from_file(xml_file, fileinfo=None, **kwargs):
     data['_source_type'] = 'XML'
     data['_source_desc'] = xml_file
     data['patient_id'] = soup.findChild('patient_id').text
+    #data['submitter_id'] = soup.findChild('submitter_id').text
     data['_source_file_uuid'] = file_id
-    ## get file meta-data (for case_id):
+    ## get file meta-data (for case_id & submitter_id):
     if fileinfo is not None:
-        data['case_id'] = fileinfo.loc[fileinfo['file_id']==file_id[0], 'case_id'].values[0]
-    else:
-        data['case_id'] = api.get_fileinfo_data(file_id=file_id)['case_id'][0]
+        fileinfo = api.get_fileinfo_data(file_id=file_id)['case_id'][0]
+    data['case_id'] = fileinfo.loc[fileinfo['file_id']==file_id[0], 'case_id'].values[0]
+    data['submitter_id'] = fileinfo.loc[fileinfo['file_id']==file_id[0], 'submitter_id'].values[0]
     return data
 
 
